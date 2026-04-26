655285947d098e7768d50c4d839276287bff4aba
Hadis Samadi <samadi.hadis@gmail.com>
2026-04-26 12:05:19 +0330

    Add username entity

---
+++ src/main/java/com/samadihadis/loanmanagementsystem/entity/Username.java
@@ -0,0 +1,28 @@
+package com.samadihadis.loanmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Username {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String password;
+
+    private String roles = "ROLE_USER";
+
+}
